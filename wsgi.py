f11f6b6c6462ccac6a492a1f607f8cd19c3b8657
Kunjan Khatri <kunjankhatri.kk@gmail.com>
2021-01-07 01:48:48 -0500

    added class

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,22 @@
 ##importing the app from main file
 from main import app
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+acc_ix, hpower_ix, cyl_ix = 2, 4, 0
+
+class CustomAttrAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, acc_on_power=True): # no *args or **kargs
+        self.acc_on_power = acc_on_power
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        acc_on_cyl = X[:, acc_ix] / X[:, cyl_ix]
+        if self.acc_on_power:
+            acc_on_power = X[:, acc_ix] / X[:, hpower_ix]
+            return np.c_[X, acc_on_power, acc_on_cyl]
+        
+        return np.c_[X, acc_on_cyl]
 
 if __name__ == '__main__': 
     app.run(debug=True)
