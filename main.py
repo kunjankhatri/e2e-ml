fc5d39d435670b2201d684ac83e38f9c5bc8d2a2
Kunjan Khatri <kunjankhatri.kk@gmail.com>
2021-07-20 22:49:27 -0400

    updated to work on AWS

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def data():
         #return jsonify(result)
         return render_template('data.html', result = result['mpg_prediction'])
 
-if __name__ == '__main__':
-    app.run(debug=True, port=9696)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=9696)
